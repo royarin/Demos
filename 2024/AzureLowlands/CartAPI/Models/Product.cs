e43f6cf2f1200e18c9c743045984b89ca314851b
Arin Roy <royarin@gmail.com>
2024-06-25 22:43:19 +0200

    refactor: Update Price property type to decimal in Product.cs

--- 2024/AzureLowlands/CartAPI/Models/Product.cs
+++ 2024/AzureLowlands/CartAPI/Models/Product.cs
@@ -10,7 +10,7 @@
         public string Filename { get; set; }
         public int Height { get; set; }
         public int Width { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
         public int Rating { get; set; }
     }
 }
